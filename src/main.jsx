a79a3b8416ba7aa1281a70d5a4e6dd5c083d7806
SatoruBtd6 <140970613+SatoruBtd6@users.noreply.github.com>
2026-09-15 23:49:22 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Auth from "./Auth.jsx";
+
+createRoot(document.getElementById("root")).render(<React.StrictMode><Auth /></React.StrictMode>);
